acae0ef6b0de774a5b41cd805a2e8429997d880d
Ben Blue <dalcowboiz@gmail.com>
2018-02-02 01:27:56 -0500

    add basic images route to return received image

--- hello.py
+++ hello.py
@@ -1,6 +1,16 @@
-from flask import Flask
+from flask import Flask, render_template, request, send_file
+from io import BytesIO
+
 app = Flask(__name__)
 
 @app.route('/')
-def hello_world():
-    return 'Hello, World!'
+def index():
+    return render_template('index.html')
+
+@app.route('/images', methods=['POST'])
+def submit_image():
+    file = request.files['file']
+    if file.filename != "":
+        return send_file(BytesIO(file.read()), file.mimetype)
+    else:
+        return "no image submitted"
